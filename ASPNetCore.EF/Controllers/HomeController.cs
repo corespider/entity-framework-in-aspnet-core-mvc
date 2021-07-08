aba31e1cbac67c406e35e9fc62f9bd0a9fda0ffe
Your Name <53593343+CoreProgramm@users.noreply.github.com>
2021-07-08 19:02:24 +0530

    Add project files.

---
+++ ASPNetCore.EF/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using ASPNetCore.EF.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCore.EF.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly EFContext _context;
+
+        public HomeController(ILogger<HomeController> logger,EFContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Customer> Listcustomers = _context.Customers.ToList();
+            return View(Listcustomers);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASPNetCore.EF/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCore.EF.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Designation { get; set; }
+    }
+}
